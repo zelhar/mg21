0e2187d3caca749158f45dd7cbf68acf62cf3d0f
Yiftach Kolb <zelhar@gmail.com>
2021-05-25 17:59:50 +0200

    S

--- temp_module.py
+++ temp_module.py
@@ -11,7 +11,7 @@ plt.ion()
 
 #%%
 # inline magic
-%pylab inline
+#%pylab inline
 
 #plot([1,2,3], [2,4,9])
 
@@ -19,8 +19,11 @@ print("lalala\n")
 
 plt.bar([1,2,3,4], [10, 20, 5, 2])
 
+plt.cla()
+
 
 plt.plot([1,2,3], [2,4,9])
+plt.cla()
 
 
 #%%
@@ -122,9 +125,9 @@ nx.draw_spring(g)
 
 nx.to_numpy_array(g)
 
-#p = "./data_netcore/CPDB_high_confidence.txt"
-p = "./data_netcore/testdata.txt"
+p = "./data_netcore/CPDB_high_confidence.txt"
 X = fromSavedEdgeList(p, digraph=False)
+
 X.edges.data('weight')
 
 A = nx.to_numpy_array(X)
@@ -136,6 +139,28 @@ x
 y = np.linalg.inv(x)
 y
 np.dot(x,y)
+
+def transitionMatrixG(G):
+    """input G: a graph.
+    output T: the transition matrix of G,
+    column normalized.
+    """
+    A = nx.to_numpy_array(G)
+    T = A.T / A.sum(axis=1)
+    return T
+
+def coreTransitionMatrixG(G):
+    """Similar to transitionMatrixG but 
+    Returns the core normalized transition matrix of G.
+    The cloumns are normalized.
+    """
+    A = nx.to_numpy_array(G)
+    coreness = nx.core_number(G)
+    coreness = np.array([coreness[k] for k in range(len(coreness))])
+    A = A * coreness
+    T = A.T / A.sum(axis=1)
+    #T.sum(axis=0)
+    return T
 
 def diffusionMatrix(T, alpha=0.2):
     """
@@ -150,20 +175,27 @@ def diffusionMatrix(T, alpha=0.2):
     K = alpha * np.linalg.inv(K)
     return K
 
-def diffusionMatrixG(G, alpha=0.2):
+def diffusionMatrixG(G, alpha=0.2, coreness=False):
     """
     input G: a networkz graph.
     input alpha: the restart parameter.
+    input bool coreness: If True, the normalization uses core number rather
+    than the standard adjacency matrix.
     Output K: the diffusion matrix, which is
     K = a [I - (1-a)T]^(-1)
     """
-    A = nx.to_numpy_array(G)
-    T = A.T / A.sum(axis=1)
+    #A = nx.to_numpy_array(G)
+    #T = A.T / A.sum(axis=1)
+    if coreness:
+        T = coreTransitionMatrixG(G)
+    else:
+        T = transitionMatrixG(G)
     n = T.shape[0]
     I = np.identity(n)
     K = I - (1 - alpha)*T
     K = alpha * np.linalg.inv(K)
     return K
+
 
 def RWR(T, alpha=0.2, q=1, epsilon=1e-6, maxiter=10**6):
     """Calculates the stationary distribution of a RWR process
@@ -218,7 +250,6 @@ def RWRG(G, alpha=0.2, q=1, epsilon=1e-6, maxiter=10**6):
 
 
 
-
 x = diffusionMatrixG(X)
 x
 
@@ -234,6 +265,103 @@ t = RWRG(X)
 t
 tt = np.dot(x, np.ones(3) / 3)
 tt
+
+## Coreness
+
+G = nx.karate_club_graph()
+
+club = ['red' if G.nodes[i]['club']=='Mr. Hi' else 'yellow' for i in G.nodes()]
+club
+
+nx.draw_spring(G, with_labels=True, node_color=club)
+
+nx.draw_spring(nx.k_shell(G), with_labels=True)
+
+nx.draw_spring(nx.k_core(G,2), with_labels=True)
+
+nx.draw_spring(nx.k_core(G,3), with_labels=True)
+
+nx.draw_spring(nx.k_core(G,4), with_labels=True)
+
+nx.draw_spring(nx.k_core(G), with_labels=True)
+
+plt.close()
+
+coreness = nx.core_number(G)
+coreness = np.array([coreness[k] for k in range(len(coreness))])
+coreness
+
+A = nx.to_numpy_array(G)
+A = A * coreness
+A
+T = A.T / A.sum(axis=1)
+T.sum(axis=0)
+
+a = coreTransitionMatrixG(G)
+a
+a == T
+
+b = transitionMatrixG(G)
+b.sum(axis=0)
+a.sum(axis=0)
+
+
+x = np.arange(16).reshape((4,4))
+y = np.array([1,-2,3,-4])
+
+
+n = len(a)
+
+plt.cla()
+nx.draw_spring(G, with_labels=True, node_color=club, node_size=np.dot(a,
+    np.ones(n)*100))
+
+plt.cla()
+nx.draw_spring(G, with_labels=True, node_color=club, node_size=np.dot(b,
+    np.ones(n)*100))
+
+plt.cla()
+nx.draw_spring(G, with_labels=True, node_color=club, node_size=100*coreness)
+
+
+plt.cla()
+nx.draw_spring(G, with_labels=True, node_color=club, node_size=100*coreness,
+        labels=nx.core_number(G))
+
+
+
+
+
+K = diffusionMatrixG(G, coreness=False)
+R = diffusionMatrixG(G, coreness=True)
+
+G.nodes.data()
+G.nodes.data('club')
+clubdict = dict( G.nodes.data('club'))
+
+plt.cla()
+nx.draw_spring(G, with_labels=True, node_color=club, node_size=10*coreness,
+        labels=clubdict)
+
+r = np.dot(R, np.ones(len(R)))
+r.sum()
+s = np.dot(K, np.ones(len(K)))
+s.sum()
+
+
+plt.cla()
+nx.draw_spring(G, with_labels=True, node_color=club, node_size=100*r,
+        labels=clubdict)
+
+plt.cla()
+nx.draw_spring(G, with_labels=True, node_color=club, node_size=100*s,
+        labels=clubdict)
+
+
+
+
+
+
 
 
 
