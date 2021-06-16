dec7e202e42ba7fdd518bba2f9368dcccdd55f0f
Yiftach Kolb <zelhar@gmail.com>
2021-06-16 18:53:31 +0200

    A

--- hic/reversals.py
+++ hic/reversals.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os.path as op
+import pygraphviz as pgv
+
+import toolz
 
 plt.ion()
 
@@ -45,3 +48,105 @@ plt.close()
 G.add_edges_from(zip("dE","eF"), weight=3, color="green")
 
 edgeColors = [d['color'] for (u,v,d) in G.edges(data=True) ]
+
+
+
+### PyGraphViz Tests
+G = pgv.AGraph(directed=False, strict=False)
+
+a_nodes = ["a"+str(i) for i in range(10)]
+a_nodes
+b_nodes = ["b"+str(i) for i in range(10)]
+b_nodes
+my_nodes = list(toolz.interleave([a_nodes, b_nodes]))
+my_nodes
+
+G.add_nodes_from(my_nodes)
+G.add_edges_from(zip(a_nodes, b_nodes), color="black", weight=10)
+G.add_edges_from(zip(b_nodes, a_nodes[1:]), color="blue", weight=10)
+G.add_edge("b9","a0", color="blue")
+
+#01234567890
+#0-6-52347890
+
+G.add_edge("b0","b6", color="green")
+G.add_edge("a5","a2", color="green")
+
+G.add_edge("a7","b7", color="green")
+
+
+
+G.draw("temp.png", prog="circo")
+
+#### more networkx tests
+G.clear()
+G = nx.MultiGraph()
+
+G.add_nodes_from(my_nodes)
+G.add_edges_from(zip(a_nodes, b_nodes), color="black", weight=10)
+G.add_edges_from(zip(b_nodes, a_nodes[1:]), color="blue", weight=10)
+G.add_edge("b9","a0", color="blue")
+
+G.add_edge("b0","b6", color="green")
+G.add_edge("a5","a2", color="green")
+G.add_edge("a7","b7", color="green")
+
+
+edgeColors = [d['color'] for (u,v,d) in G.edges(data=True) ]
+
+plt.cla()
+
+nx.draw_circular(G, with_labels=True, edge_color=edgeColors,
+        connectionstyle='arc3,rad=0.2')
+
+pos = nx.circular_layout(G)
+pos
+
+plt.cla()
+
+nx.draw_networkx(G, pos, node_size=50, font_size=10, node_shape="s")
+
+nx.draw_networkx_nodes(G,pos, node_shape="s", label= nx.draw_networkx_labels(G,
+    pos, font_size=8), node_color=["tan", "teal"]*10, node_size=50,)
+
+blackedges = list(zip(a_nodes, b_nodes))
+blackedges
+bnames = ["s"+str(i) for i in range(len(blackedges))]
+
+blueEdges = list(zip(b_nodes[-1:]+b_nodes[:-1], a_nodes))
+blueEdges
+
+bdict = dict(zip(blackedges, bnames))
+
+nx.draw_networkx_edges(G, pos,
+        edgelist=list(zip(a_nodes, b_nodes)),
+        edge_color="black",
+        label=nx.draw_networkx_edge_labels(G,pos,edge_labels=bdict), )
+
+nx.draw_networkx_edges(G, pos,
+        edgelist=blueEdges,
+        edge_color="blue",
+        )
+
+#nx.draw_networkx_edges(G, pos,
+#        edgelist=G.edges(),
+#        edge_color=edgeColors)
+#
+
+for a,b in zip(a_nodes,b_nodes):
+    G.edges[a,b,0]['label'] = "S"+a[1]
+    print(G[a][b])
+
+# convert to a graphviz agraph
+A = nx.nx_agraph.to_agraph(G)
+
+# write to dot file
+A.write("k5_attributes.dot")
+
+A.layout('circo')
+
+A.draw('foo.png')
+
+
+
+
